e0a3577f88100d6deb5bae750993d94786c90b9b
meganm672 <meganm672@gmail.com>
2023-10-10 10:12:09 -0400

    tried to complete the get requests

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ const app = express();
 
 const PORT = 8080;
 
-// GET - / - returns homepage
-app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
+//import morgan libary
+const morgan = require('morgan')
 
-});
+//add morgan middleware library
+app.use(morgan('dev'));
+
+ // serve up the public folder as static index.html file
+app.use(express.static('public'));
+
+// // GET - / - returns homepage
+// app.get('/', (req, res) => {
+//     // serve up the public folder as static index.html file
+    
+// });
 
 // hello world route
 app.get('/api', (req, res) => {
@@ -21,31 +30,31 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets)
 });
 
-// get pet by owner with query string
+// get pet by owner with query string ?owner = owenerName
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const { owner } = req.query
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.send("Returning response with the query param 'owner' set to " + pet)
 });
 
-// get pet by name
+// get pet by name like /fido
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
-
+    const { name } = req.params
+    
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
-
+  
     // send the pet as a response
-
+    res.send('Returting response to request for the dynamic route: '+ pet)
 });
 
 app.listen(PORT, () => {
